53af9f1165ab7dc2888e7d95a1de0d90561188d0
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-03-04 10:11:05 -0800

    madness

--- stone.lua
+++ stone.lua
@@ -46,7 +46,15 @@ function underch.stone.register_stone(name, id, mossy)
 	-----------------------------
 	-----------TEST--------------
 	-----------------------------
-	underch.ores.register_ore(name .. " Coal", id .. "_coal", "default_mineral_coal.png", "default:coal", "underch_" .. id .. ".png", 1)
+	underch.ores.register_ore(name .. " Coal", id .. "_coal", "default_mineral_coal.png", "default:coal", "underch_" .. id .. ".png", 3)
+	underch.ores.register_ore(name .. " Tin", id .. "_tin", "default_mineral_tin.png", "default:tin_lump", "underch_" .. id .. ".png", 2)
+	underch.ores.register_ore(name .. " Gold", id .. "_gold", "default_mineral_gold.png", "default:gold_lump", "underch_" .. id .. ".png", 2)
+	underch.ores.register_ore(name .. " Iron", id .. "_iron", "default_mineral_iron.png", "default:iron_lump", "underch_" .. id .. ".png", 2)
+	underch.ores.register_ore(name .. " Mese", id .. "_mese", "default_mineral_mese.png", "default:mese_lump", "underch_" .. id .. ".png", 1)
+	underch.ores.register_ore(name .. " Copper", id .. "_copper", "default_mineral_copper.png", "default:copper_lump", "underch_" .. id .. ".png", 2)
+	underch.ores.register_ore(name .. " Diamond", id .. "_diamond", "default_mineral_diamond.png", "default:diamond", "underch_" .. id .. ".png", 1)
+
+
 	--crafts
 	minetest.register_craft({
 		output = brick .. " 4",
