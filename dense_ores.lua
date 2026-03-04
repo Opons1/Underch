5d21c8b69c12dd14aefc46e336bd1b59ec5378d2
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-03-04 09:34:32 -0800

    add to ores

--- dense_ores.lua
+++ dense_ores.lua
@@ -1,10 +1,25 @@
-underch.dense_ores = {}
+underch.ores = {}
+--registers an ore
+function underch.ores.register_ore(name, id, texture, item, level, stonetexture, level)
+	stonetexture = stonetexture or "default_stone.png"
+	level = level or 1
+	core.register_node("underch:" .. id .. "_ore", {
+		description = name .. " Ore",
+		tiles = {stonetexture .. "^" .. texture},
+		groups = {cracky = level},
+		drop = item,
+		sounds = default.node_sound_stone_defaults(),
+	})
+end
 
-function underch.dense_ores.register_ore(name, id, texture, item)
-	minetest.register_node("underch:" .. id .. "_dense_ore", {
-		description = name .. " Dense Ore",
-		tiles = {"default_stone.png^" .. texture .. "^(" .. texture .. "^[transform7)"},
-		groups = {cracky = 1, jit_shadow = 1},
+--registers a dense ore
+function underch.ores.register_dense_ore(name, id, texture, item, stonetexture, level)
+	stonetexture = stonetexture or "default_stone.png"
+	level = level or 1
+	core.register_node("underch:" .. id .. "_dense_ore", {
+		description = "Dense " .. name .. " Ore",
+		tiles = {stonetexture.. "^" .. texture .. "^(" .. texture .. "^[transform7)"},
+		groups = {cracky = level},
 		 
 		drop = {
 			items = {{items = {item .. " 2"}}, {items = {item}, rarity = 2}}
@@ -13,7 +28,8 @@ function underch.dense_ores.register_ore(name, id, texture, item)
 	})
 end
 
-underch.dense_ores.register_ore("Coal", "coal", "default_mineral_coal.png", "default:coal_lump");
-underch.dense_ores.register_ore("Iron", "iron", "default_mineral_iron.png", "default:iron_lump");
-underch.dense_ores.register_ore("Copper", "copper", "default_mineral_copper.png", "default:copper_lump");
-underch.dense_ores.register_ore("Gold", "gold", "default_mineral_gold.png", "default:gold_lump");
+
+underch.ores.register_dense_ore("Coal", "coal", "default_mineral_coal.png", "default:coal_lump");
+underch.ores.register_dense_ore("Iron", "iron", "default_mineral_iron.png", "default:iron_lump");
+underch.ores.register_dense_ore("Copper", "copper", "default_mineral_copper.png", "default:copper_lump");
+underch.ores.register_dense_ore("Gold", "gold", "default_mineral_gold.png", "default:gold_lump");
