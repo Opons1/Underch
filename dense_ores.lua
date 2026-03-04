c4db0868ce0725effe25b0e5f75c08ddc854c7dd
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-03-04 10:31:20 -0800

    make better ores for gems

--- dense_ores.lua
+++ dense_ores.lua
@@ -28,8 +28,21 @@ function underch.ores.register_dense_ore(name, id, texture, item, stonetexture, 
 	})
 end
 
-
+--old, still in use
 underch.ores.register_dense_ore("Coal", "coal", "default_mineral_coal.png", "default:coal_lump");
 underch.ores.register_dense_ore("Iron", "iron", "default_mineral_iron.png", "default:iron_lump");
 underch.ores.register_dense_ore("Copper", "copper", "default_mineral_copper.png", "default:copper_lump");
 underch.ores.register_dense_ore("Gold", "gold", "default_mineral_gold.png", "default:gold_lump");
+--new gems yayayyayayay
+--Amethyst
+underch.ores.register_ore("Amethyst", "amethyst", "underch_amethyst_ore.png", "underch:amethyst", "underch_diorite.png", 2)
+--ruby
+underch.ores.register_ore("Ruby", "ruby", "underch_ruby_ore.png", "underch:ruby", "underch_granite.png", 2)
+--emerald
+underch.ores.register_ore("Emerald", "emerald", "underch_emerald_ore.png", "underch:emerald", "underch_phonolite.png", 2)
+--saphire
+underch.ores.register_ore("Saphire", "saphire", "underch_saphire_ore.png", "underch:saphire", "underch_gneiss.png", 2)
+--Quartz
+underch.ores.register_ore("Quartz", "quartz", "underch_quartz_ore.png", "underch:quartz", "underch_diorite.png", 2)
+--Aquamarine
+underch.ores.register_ore("Aquamarine", "aquamarine", "underch_aquamarine_ore.png", "underch:aquamarine", "underch_diorite.png", 2)
