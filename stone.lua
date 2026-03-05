833d6fd94cd9c7e8177c5678be6c2652363b1c0c
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-03-05 09:29:57 -0800

    finish quartz and blox support

--- stone.lua
+++ stone.lua
@@ -44,9 +44,10 @@ function underch.stone.register_stone(name, id, mossy)
 		groups = {cracky = 2, stone = 1, stoneblock = 1},
 		sounds = default.node_sound_stone_defaults(),
 	})
-	-----------------------------
-	-----------TEST--------------
-	-----------------------------
+	--ORES IN THE STONE
+	--MANY MANY NODES
+	--VERY FUN
+	underch.stone.defs[id].ores = {}
 	--normal
 	underch.ores.register_ore(name.." Coal",    id .. "_coal",    "default_mineral_coal.png",     "default:coal",       "underch_" .. id .. ".png", 3)
 	underch.ores.register_ore(name.." Tin",     id .. "_tin",     "default_mineral_tin.png",     "default:tin_lump",    "underch_" .. id .. ".png", 2)
@@ -56,7 +57,6 @@ function underch.stone.register_stone(name, id, mossy)
 	underch.ores.register_ore(name.." Copper",  id .. "_copper",  "default_mineral_copper.png",  "default:copper_lump", "underch_" .. id .. ".png", 2)
 	underch.ores.register_ore(name.." Diamond", id .. "_diamond", "default_mineral_diamond.png", "default:diamond",     "underch_" .. id .. ".png", 1)
 	--logging the ores, debating whether this should be a for loop taking in a table or this monstrosity
-	underch.stone.defs[id].ores = {}
 	underch.stone.defs[id].ores[core.get_content_id("default:stone_with_coal")]    = core.get_content_id(base.."_coal_ore")
 	underch.stone.defs[id].ores[core.get_content_id("default:stone_with_tin")]     = core.get_content_id(base.."_tin_ore")
 	underch.stone.defs[id].ores[core.get_content_id("default:stone_with_gold")]    = core.get_content_id(base.."_gold_ore")
@@ -68,7 +68,7 @@ function underch.stone.register_stone(name, id, mossy)
 	--moreores:mineral_silver
 	--moreores:mineral_mithril
 	if underch.have_moreores then 
-	underch.ores.register_ore(name.." Silver", id  .. "_moreores_silver", "moreores_mineral_silver.png",   "moreores:silver_lump",  "underch_" .. id .. ".png", 2)
+	underch.ores.register_ore(name.." Silver", id  .. "_moreores_silver",  "moreores_mineral_silver.png",  "moreores:silver_lump",  "underch_" .. id .. ".png", 2)
 	underch.ores.register_ore(name.." Mithril", id .. "_moreores_mithril", "moreores_mineral_mithril.png", "moreores:mithril_lump", "underch_" .. id .. ".png", 1)
 	underch.stone.defs[id].ores[core.get_content_id("moreores:mineral_mithril")] = core.get_content_id(base.."_moreores_mithril_ore")
 	underch.stone.defs[id].ores[core.get_content_id("moreores:mineral_silver")]  = core.get_content_id(base.."_moreores_silver_ore")
@@ -130,26 +130,26 @@ function underch.stone.register_stone(name, id, mossy)
 	underch.ores.register_ore(name.." Sapphire", id .. "_sapphire_sapphire", "sapphire_ore.png", "sapphire:sapphire_gem", "underch_" .. id .. ".png", 2)
 	underch.stone.defs[id].ores[core.get_content_id("sapphire:sapphire_ore")] = core.get_content_id(base.."_sapphire_sapphire_ore")
 	end
+	--TERUMET
 	if underch.have_terumet then
-	underch.ores.register_ore(name.." Terumetal", id .. "_terumet_terumetal", "terumet_ore_raw.png", "terumet:lump_raw", "underch_" .. id .. ".png", 2)
+	underch.ores.register_ore(name.." Terumetal",       id .. "_terumet_terumetal",       "terumet_ore_raw.png",       "terumet:lump_raw", "underch_" .. id .. ".png", 2)
 	underch.ores.register_ore(name.." Dense Terumetal", id .. "_terumet_dense_terumetal", "terumet_ore_dense_raw.png", "terumet:lump_raw", "underch_" .. id .. ".png", 1)
 	underch.stone.defs[id].ores[core.get_content_id("terumet:ore_raw")] = core.get_content_id(base.."_terumet_terumetal_ore")
 	underch.stone.defs[id].ores[core.get_content_id("terumet:ore_dense_raw")] = core.get_content_id(base.."_terumet_dense_terumetal_ore")
-	end
 	core.override_item("underch:" .. id .. "_terumet_dense_terumetal_ore", {
 		drop = "terumet:lump_raw 5",
 	})
-	--dense
-	--[[
-	underch.ores.register_dense_ore(name .. " Coal", id .. "_coal", "default_mineral_coal.png", "default:coal", "underch_" .. id .. ".png", 3)
-	underch.ores.register_dense_ore(name .. " Tin", id .. "_tin", "default_mineral_tin.png", "default:tin_lump", "underch_" .. id .. ".png", 2)
-	underch.ores.register_dense_ore(name .. " Gold", id .. "_gold", "default_mineral_gold.png", "default:gold_lump", "underch_" .. id .. ".png", 2)
-	underch.ores.register_dense_ore(name .. " Iron", id .. "_iron", "default_mineral_iron.png", "default:iron_lump", "underch_" .. id .. ".png", 2)
-	underch.ores.register_dense_ore(name .. " Mese", id .. "_mese", "default_mineral_mese.png", "default:mese_lump", "underch_" .. id .. ".png", 1)
-	underch.ores.register_dense_ore(name .. " Copper", id .. "_copper", "default_mineral_copper.png", "default:copper_lump", "underch_" .. id .. ".png", 2)
-	underch.ores.register_dense_ore(name .. " Diamond", id .. "_diamond", "default_mineral_diamond.png", "default:diamond", "underch_" .. id .. ".png", 1)
-	]]
-	--crafts
+	end
+	--QUARTZ
+	if underch.have_quartz then
+	underch.ores.register_ore(name.." Quartz", id .. "_quartz_quartz", "quartz_ore.png", "quartz:quartz_crystal", "underch_" .. id .. ".png", 2)
+	underch.stone.defs[id].ores[core.get_content_id("quartz:quartz_ore")] = core.get_content_id(base.."_quartz_quartz_ore")
+	end
+	--BLOX
+	if underch.have_blox then
+	underch.ores.register_ore(name.." Glow", id .. "_blox_glow", "blox_glowore.png", "blox:glowdust", "underch_" .. id .. ".png", 2)
+	underch.stone.defs[id].ores[core.get_content_id("blox:glowore")] = core.get_content_id(base.."_blox_glow_ore")
+	end
 	core.register_craft({
 		output = brick .. " 4",
 		recipe = {
